5edc2d6fbc1a9e889f33dc2540c89024463d5125
Àlex Correa Tomàs <alex.correa@est.fib.upc.edu>
2017-02-10 14:36:47 +0100

    Starting GUI

---
+++ src/DiskGUI.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Created by Àlex on 10/2/2017.
+ */
+public class DiskGUI {
+    private JRadioButton firstComeFirstServedRadioButton;
+    private JRadioButton shortestSeekTimeFirstRadioButton;
+    private JRadioButton SCANDiskElevatorRadioButton;
+    private JSlider slider1;
+}
