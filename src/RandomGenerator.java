c2d752e75ec4fd3f437808b4ac29bfd8a3f43710
gabbberland <oriol.giralt@yahoo.es>
2017-02-10 14:18:25 +0100

    radom generator

--- src/RandomGenerator.java
+++ src/RandomGenerator.java
@@ -1,7 +1,23 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Created by oriol on 10/02/17.
  */
 public class RandomGenerator {
-    public Integer[]
-    public static Integer[] GenerateRandoms
+    public ArrayList<Integer> numbers = GenerateRandoms();
+
+    private static ArrayList<Integer> GenerateRandoms(){
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        int it = 0;
+        while(it < 20){
+            Random randomGenerator = new Random();
+            int n = randomGenerator.nextInt(100);
+            if (!array.contains(n)) {
+                array.add(n);
+                ++it;
+            }
+        }
+        return array;
+    }
 }
