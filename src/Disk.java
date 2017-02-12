4e7eb832637a0423703987172747fc99a644b84e
gabbberland <oriol.giralt@yahoo.es>
2017-02-12 15:12:59 +0100

    Disk class implemented random removed

--- src/Disk.java
+++ src/Disk.java
@@ -1,5 +1,43 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Created by oriol on 10/02/17.
  */
 public class Disk {
+    private Integer index;
+    private ArrayList<Integer> numbers;
+
+    Disk(){
+        index = 0;
+        numbers = InitRandoms();
+    }
+
+    public void addRandom(){
+        if (numbers.size() < 20){
+            Random randomGenerator = new Random();
+            int n = randomGenerator.nextInt(100);
+            if (!numbers.contains(n))
+                numbers.add(n);
+        }
+    }
+
+    public void deleteValue(Integer val){
+        if(numbers.contains(val))
+            numbers.remove(val);
+    }
+
+    private static ArrayList<Integer> InitRandoms(){
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        int it = 0;
+        while(it < 20){
+            Random randomGenerator = new Random();
+            int n = randomGenerator.nextInt(100);
+            if (!array.contains(n)) {
+                array.add(n);
+                ++it;
+            }
+        }
+        return array;
+    }
 }
