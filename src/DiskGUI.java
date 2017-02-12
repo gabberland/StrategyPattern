ef35ea3eb41df3752c5932e637a6d3f550646d52
gabbberland <oriol.giralt@yahoo.es>
2017-02-12 19:34:59 +0100

    algorithms done!

--- src/DiskGUI.java
+++ src/DiskGUI.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /**
  * Created by Àlex on 10/2/2017.
@@ -8,4 +10,14 @@ public class DiskGUI {
     private JRadioButton shortestSeekTimeFirstRadioButton;
     private JRadioButton SCANDiskElevatorRadioButton;
     private JSlider slider1;
+
+    public DiskGUI() {
+        firstComeFirstServedRadioButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                super.mouseClicked(mouseEvent);
+
+            }
+        });
+    }
 }
